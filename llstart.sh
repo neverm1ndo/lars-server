054affdfe638316bbf25d7c3a3073216475b3c2f
neverm1ndo <reddigitalsw@gmail.com>
2021-01-12 17:28:06 +0300

    add launch sh script

---
+++ llstart.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+## LLS launch script
+cd /home/nmnd/libertylogs
+RED="\e[31m"
+NC="\e[0m"
+GREEN="\e[32m"
+LYELLOW="\e[93m"
+check () {
+	if 
+		[[ -n "`pgrep screen`" ]];
+	then
+		return 1
+	else
+		return 0
+	fi
+
+}
+launch() {
+	if check ; then
+		echo " > Запуск LL сервера..."
+		if 
+			[[ -n "`pgrep mongod`" ]]; 
+		then 
+			echo " > Сервер MongoDB уже запущен"
+			screen -d -m -S lls npm run server
+		else
+			echo " > Запуск сервера MongoDB..."
+			mongod | 
+	       		echo " > Сервер MongoDB запущен" |
+			screen -d -m -S lls npm run server
+		fi
+		sleep 2s
+		if check;
+		then
+			echo -e " > > > ${RED}Не удалось запустить сервер LL!${NC}"
+		else
+			echo -e " > > > ${GREEN}Сервер LL успешно запущен${NC}"
+		fi
+	else
+	       echo -e " > > > ${LYELLOW} LL сервер уже запущен ${NC}"
+	fi
+}
+trap launch 0 
+
+
+		
