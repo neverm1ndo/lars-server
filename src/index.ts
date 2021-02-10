08e9ca65c059fa2196e9158927602c586657e8f8
neverm1ndo <it@nmnd.ru>
2021-02-11 01:48:34 +0300

    ?

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import './pre-start'; // Must be the first import
 import app from '@server';
-
-require("greenlock-express")
-    .init({
-        packageRoot: process.env.PKG_ROOT,
-        configDir: "./greenlock.d",
-        maintainerEmail: "it@nmnd.ru",
-        cluster: false
-    }).serve(app)
+import https from 'https';
 
 // Start the server
-// const port = Number(process.env.HTTP_PORT || 3000);
-// app.listen(port, () => { console.log(' DEV HTTP LLS listening on port', port) });
+const port = Number(process.env.HTTP_PORT || 3000);
+https.createServer({
+  key: process.env.SSL_KEY!,
+  cert: process.env.SSL_CERT!
+}, app).listen(port, () => { console.log(' DEV HTTP LLS listening on port', port) });
