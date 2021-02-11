b85d4b2ce38caa4312cdcd23508a49edc5b1c62a
neverm1ndo <it@nmnd.ru>
2021-02-12 01:13:27 +0300

    remove request cert

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,7 @@ https.createServer({
   key: readFileSync(process.env.SSL_KEY!, 'utf8'),
   cert: readFileSync(process.env.SSL_CERT!, 'utf8'),
   ca: readFileSync(process.env.SSL_CA!, 'utf8'),
-  rejectUnauthorized: false,
-  requestCert: true
+  rejectUnauthorized: false
 }, app).listen(process.env.HTTPS_PORT, () => { console.log('HTTPS LLS NODE listening on port', process.env.HTTPS_PORT) });
 
  app.listen(process.env.HTTP_PORT, () => { console.log('HTTP LLS NODE listening on port', process.env.HTTP_PORT) });
