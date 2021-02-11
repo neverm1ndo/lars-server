72418c46ffbede0b6d81407b4a324ba681a958ac
neverm1ndo <it@nmnd.ru>
2021-02-12 01:01:07 +0300

    fix ssl agent

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ import { readFileSync } from 'fs';
 // Start the server
 https.createServer({
   key: readFileSync(process.env.SSL_KEY!, 'utf8'),
-  cert: readFileSync(process.env.SSL_CERT!, 'utf8')
+  cert: readFileSync(process.env.SSL_CERT!, 'utf8'),
+  ca: readFileSync(process.env.SSL_CA!, 'utf8'),
+  rejectUnauthorized: false,
+  requestCert: true
 }, app).listen(process.env.HTTPS_PORT, () => { console.log('HTTPS LLS NODE listening on port', process.env.HTTPS_PORT) });
 
  app.listen(process.env.HTTP_PORT, () => { console.log('HTTP LLS NODE listening on port', process.env.HTTP_PORT) });
