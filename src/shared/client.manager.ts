5436c343958d10c739208164d37acd217163e57e
neverm1ndo <it@nmnd.ru>
2021-02-18 00:06:51 +0300

    update(client-manager) upade functions, add Client interface

--- src/shared/client.manager.ts
+++ src/shared/client.manager.ts
@@ -1,16 +1,37 @@
 import { WSMessage } from '@interfaces/ws.message';
+import { User } from '@interfaces/user';
+
+export interface Client {
+  user: User;
+  ws: WebSocket;
+  action?: {
+    type: 'redacting' | 'idle';
+    path?: string;
+  };
+}
 
 export class ClientManager {
-  pool: WebSocket[] = [];
-  add(client: WebSocket) {
+
+  pool: Client[] = [];
+
+  add(client: Client) {
     this.pool.push(client);
   }
-  remove(client: WebSocket) {
-    this.pool.splice(this.pool.indexOf(client) , 1);
+  remove(ws: WebSocket) {
+    new Promise<Client>((resolve: any, reject: any) => {
+        this.pool.forEach((client: Client) => {
+          if (client.ws == ws) {
+            resolve(client);
+          }
+        });
+        reject();
+    }).then((client: Client) => {
+      this.pool.splice(this.pool.indexOf(client) , 1);
+    }).catch(() => { console.error('[client-manager] client not exists')})
   }
   sendall(message: WSMessage) {
-    this.pool.forEach((client: WebSocket) => {
-      client.send(JSON.stringify(message));
+    this.pool.forEach((client: Client) => {
+      client.ws.send(JSON.stringify(message));
     });
   }
 }
