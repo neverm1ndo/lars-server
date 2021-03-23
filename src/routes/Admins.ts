a0b4fcbca262d5e1c1bd6557cc4f13b4ddd20487
neverm1ndo <it@nmnd.ru>
2021-03-24 02:44:56 +0300

    add(route) add new route /all in admins

--- src/routes/Admins.ts
+++ src/routes/Admins.ts
@@ -22,6 +22,20 @@ router.get('/list', corsOpt, (req: any, res: any) => {
       Logger.log('error', err);
     });
 });
+router.get('/all', corsOpt, (req: any, res: any) => {
+  if (!req.headers.authorization && req.user.gr !== 10) return res.sendStatus(UNAUTHORIZED);
+  Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user,`role: ${req.user.group_id}`, '-> ADMIN_LIST [', req.originalUrl, ']');
+  MSQLPool.promise()
+    .query("SELECT * FROM phpbb_users WHERE group_id IN (?, ?, ?, ?, ?, ?)", [9, 10, 11, 12, 13, 14])
+    .then(([rows]: any[]): void => {
+      res.send(JSON.stringify(rows));
+    })
+    .catch((err: any): void => {
+      res.status(UNAUTHORIZED).send(err);
+      Logger.log('error', `[${req.connection.remoteAddress}]`, 401, req.user.user, 'Failed admin list query');
+      Logger.log('error', err);
+    });
+});
 router.get('/change-status', corsOpt, (req: any, res: any) => {
   if (!req.headers.authorization && req.user.gr !== 10) return res.sendStatus(UNAUTHORIZED);
   Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user,`role: ${req.user.group_id}`, '-> CHANGE_ADMIN_STATUS', req.query.path, '[', req.originalUrl, ']');
