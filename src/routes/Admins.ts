c8793652009a19eaa7ea16e47fc385463fa2b549
neverm1ndo <it@nmnd.ru>
2021-03-25 01:10:51 +0300

    fix(admins)add put query change admin group

--- src/routes/Admins.ts
+++ src/routes/Admins.ts
@@ -1,6 +1,7 @@
 import StatusCodes from 'http-status-codes';
 import { Router } from 'express';
 import { Logger } from '@shared/Logger';
+import bodyParser from 'body-parser';
 
 import { corsOpt, MSQLPool } from '@shared/constants';
 
@@ -12,7 +13,7 @@ router.get('/list', corsOpt, (req: any, res: any) => {
   if (!req.headers.authorization && req.user.gr !== 10) return res.sendStatus(UNAUTHORIZED);
   Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user,`role: ${req.user.group_id}`, '-> ADMIN_LIST [', req.originalUrl, ']');
   MSQLPool.promise()
-    .query("SELECT username, user_id, user_avatar, group_id FROM phpbb_users WHERE group_id IN (?, ?, ?, ?, ?, ?)", [9, 10, 11, 12, 13, 14])
+    .query("SELECT username, user_id, user_regdate, user_email, user_avatar, group_id FROM phpbb_users WHERE group_id IN (?, ?, ?, ?, ?, ?)", [9, 10, 11, 12, 13, 14])
     .then(([rows]: any[]): void => {
       res.send(JSON.stringify(rows));
     })
@@ -36,11 +37,11 @@ router.get('/all', corsOpt, (req: any, res: any) => {
       Logger.log('error', err);
     });
 });
-router.get('/change-status', corsOpt, (req: any, res: any) => {
+router.put('/change-group', bodyParser.json(), corsOpt, (req: any, res: any) => {
   if (!req.headers.authorization && req.user.gr !== 10) return res.sendStatus(UNAUTHORIZED);
-  Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user,`role: ${req.user.group_id}`, '-> CHANGE_ADMIN_STATUS', req.query.path, '[', req.originalUrl, ']');
+  Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user,`role: ${req.user.group_id}`, '-> CHANGE_ADMIN_GROUP', `${req.body.username} : ${req.body.group}`, req.query.path, '[', req.originalUrl, ']');
   MSQLPool.promise()
-    .query("UPDATE phpbb_users SET group_id = ? WHERE username = ?", [req.query.group, req.query.username])
+    .query("UPDATE phpbb_users SET group_id = ? WHERE username = ?", [req.body.group, req.body.username])
     .then(([rows]: any[]): void => {
       res.send(JSON.stringify(rows));
     })
