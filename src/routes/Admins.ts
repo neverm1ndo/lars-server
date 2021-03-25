47711fd678a46295353ee68284e95fbaa282087c
neverm1ndo <it@nmnd.ru>
2021-03-25 03:01:00 +0300

    fix(admins) fix user avatar link

--- src/routes/Admins.ts
+++ src/routes/Admins.ts
@@ -7,7 +7,7 @@ import { corsOpt, MSQLPool } from '@shared/constants';
 
 const router = Router();
 
-const { UNAUTHORIZED } = StatusCodes;
+const { OK, UNAUTHORIZED } = StatusCodes;
 
 router.get('/list', corsOpt, (req: any, res: any) => {
   if (!req.headers.authorization && req.user.gr !== 10) return res.sendStatus(UNAUTHORIZED);
@@ -15,6 +15,13 @@ router.get('/list', corsOpt, (req: any, res: any) => {
   MSQLPool.promise()
     .query("SELECT username, user_id, user_regdate, user_email, user_avatar, group_id FROM phpbb_users WHERE group_id IN (?, ?, ?, ?, ?, ?)", [9, 10, 11, 12, 13, 14])
     .then(([rows]: any[]): void => {
+      for (let i = 0; i > rows.length; i++) {
+        if (rows[i].user_avatar == '') {
+          rows[i].user_avatar = 'https://www.gta-liberty.ru/styles/prosilver_ex/theme/images/no_avatar.gif';
+        } else {
+          rows[i].user_avatar = 'https://www.gta-liberty.ru/images/avatars/upload/' + rows[i].user_avatar;
+        }
+      }
       res.send(JSON.stringify(rows));
     })
     .catch((err: any): void => {
@@ -43,7 +50,7 @@ router.put('/change-group', bodyParser.json(), corsOpt, (req: any, res: any) => 
   MSQLPool.promise()
     .query("UPDATE phpbb_users SET group_id = ? WHERE username = ?", [req.body.group, req.body.username])
     .then(([rows]: any[]): void => {
-      res.send(JSON.stringify(rows));
+      res.send(OK);
     })
     .catch((err: any): void => {
       res.status(UNAUTHORIZED).send(err);
