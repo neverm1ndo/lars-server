dd61fb4a8d71d90ac5acd728fbac90cc964bde48
neverm1ndo <it@nmnd.ru>
2021-03-25 03:07:29 +0300

    fix(admin) fix user avatar condition

--- src/routes/Admins.ts
+++ src/routes/Admins.ts
@@ -16,7 +16,7 @@ router.get('/list', corsOpt, (req: any, res: any) => {
     .query("SELECT username, user_id, user_regdate, user_email, user_avatar, group_id FROM phpbb_users WHERE group_id IN (?, ?, ?, ?, ?, ?)", [9, 10, 11, 12, 13, 14])
     .then(([rows]: any[]): void => {
       for (let i = 0; i > rows.length; i++) {
-        if (rows[i].user_avatar == '') {
+        if (!rows[i].user_avatar) {
           rows[i].user_avatar = 'https://www.gta-liberty.ru/styles/prosilver_ex/theme/images/no_avatar.gif';
         } else {
           rows[i].user_avatar = 'https://www.gta-liberty.ru/images/avatars/upload/' + rows[i].user_avatar;
