79819bd7b6d71afc4d8f3aa3a044557f60723ea5
neverm1ndo <it@nmnd.ru>
2021-03-30 11:27:14 +0300

    update client manager

--- src/shared/client.manager.ts
+++ src/shared/client.manager.ts
@@ -1,50 +1,55 @@
 import { WSMessage } from '@interfaces/ws.message';
 import { User } from '@interfaces/user';
-import { decodeToken } from '@shared/functions';
+
+type UserAction = 'redacting' | 'idle' | 'inlogs' | 'inmaps';
 
 export interface Client {
   user: User;
   ws: WebSocket;
   action?: {
-    type: 'redacting' | 'idle';
+    type: UserAction;
     path?: string;
   };
 }
 
 export class ClientManager {
 
-  pool: WebSocket[] = [];
+  pool: Client[] = [];
 
-  add(client: WebSocket) {
+  add(client: Client) {
     this.pool.push(client);
   }
   remove(client: WebSocket) {
-    this.pool.splice(this.pool.indexOf(client) , 1);
-    // new Promise<Client>((resolve: any, reject: any) => {
-    //     this.pool.forEach((client: Client) => {
-    //       if (client.ws == ws) {
-    //         resolve(client);
-    //       }
-    //     });
-    //     reject();
-    // }).then((client: Client) => {
-    //   this.pool.splice(this.pool.indexOf(client) , 1);
-    // }).catch(() => { console.error('[client-manager] client not exists')})
+    // this.pool.splice(this.pool.indexOf(client) , 1);
+    new Promise<Client>((resolve: any, reject: any) => {
+        this.pool.forEach((poolClient: Client) => {
+          if (client == poolClient.ws) {
+            resolve(client);
+          }
+        });
+        reject();
+    }).then((client: Client) => {
+      this.pool.splice(this.pool.indexOf(client) , 1);
+    }).catch(() => { console.error('[client-manager] client not exists')})
+  }
+  updateClientAction(ws: WebSocket, action: UserAction) {
+    this.pool.forEach((client: Client, index: number) => {
+      if (client.ws == ws) {
+        this.pool[index].action!.type = action;
+        this.sendall({ event: 'user-activity', msg: JSON.stringify({ user: client.user.name, action: action })})
+      }
+    });
   }
   closeSession(username: string) {
-    this.pool.forEach((client: WebSocket) => {
-      const url = new URL(client.url);
-      const token = url.searchParams.get('token');
-      if (token) {
-        if (username == decodeToken(token)?.name!) {
-          client.send(JSON.stringify({ event: 'expire-token', message: 'You session closed by admin' }));
-        }
+    this.pool.forEach((client: Client) => {
+      if (username == client.user.name) {
+        client.ws.send(JSON.stringify({ event: 'expire-token', message: 'You session closed by admin' }));
       }
     })
   }
   sendall(message: WSMessage) {
-    this.pool.forEach((client: WebSocket) => {
-      client.send(JSON.stringify(message));
+    this.pool.forEach((client: Client) => {
+      client.ws.send(JSON.stringify(message));
     });
   }
 }
