7b65a33e7e18c4f9df743e2a85164c7ec1f37f95
neverm1ndo <it@nmnd.ru>
2021-12-21 15:14:04 +0300

    (fix backuper) fix regexes

--- src/Backuper.ts
+++ src/Backuper.ts
@@ -1,13 +1,14 @@
 import { copy } from 'fs-extra';
+import { join } from 'path'
 
 export default class Backuper {
   constructor() {}
   static backup(path: string): Promise<void> {
     const pathsplit = path.split(new RegExp(/(\/|\\)/g));
-    const filename = pathsplit[pathsplit.length - 1];
-    const ext = filename.split(new RegExp(/.(.*)/));
+    const filename = pathsplit[pathsplit.length - 1].split('.')[0];
+    const ext = pathsplit[pathsplit.length - 1].replace(new RegExp(/(.*)\./), '');
     return new Promise((res, rej) => {
-      return copy(path, process.env.BACKUPS_PATH! + `${filename}_${new Date()}${ext}`, (err) => {
+      return copy(path, join(process.env.BACKUPS_PATH!, `${filename}_${Date.now()}.${ext}`), (err) => {
           return (!!err ? rej(err) : res());
       });
     });
