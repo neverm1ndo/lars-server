7d1005f5ed038719d7caaa4be7089aa4dd5a3162
neverm1ndo <it@nmnd.ru>
2021-12-29 16:36:29 +0300

    (add backups-router) add GET backup-file route

--- src/routes/Backups.ts
+++ src/routes/Backups.ts
@@ -19,7 +19,7 @@ const { OK, UNAUTHORIZED, INTERNAL_SERVER_ERROR, CONFLICT } = StatusCodes;
 
 const router = Router();
 
-router.get('/backups-list', corsOpt, (req: any, res: any) => { // GET download config file
+router.get('/backups-list', corsOpt, (req: any, res: any) => { // GET all live backups list
   if (!req.headers.authorization)  { res.sendStatus(UNAUTHORIZED).end('Empty authorization token'); return ; }
   if (req.user.group_id !== DEV && req.user.group_id !== BACKUPER) { res.sendStatus(UNAUTHORIZED).end('Access denied for workgroup: ' + isWorkGroup(req.user.group_id)); return; }
   Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user, `role: ${req.user.group_id}`, '-> BACKUPS_LIST', '[', req.originalUrl, ']');
@@ -28,7 +28,18 @@ router.get('/backups-list', corsOpt, (req: any, res: any) => { // GET download c
     res.send(data);
   })
 });
-router.get('/restore-backup', corsOpt, (req: any, res: Response) => {
+router.get('/backup-file', corsOpt, (req: any, res: any) => { // GET backup file
+  if (!req.headers.authorization)  { res.sendStatus(UNAUTHORIZED).end('Empty authorization token'); return ; }
+  if (req.user.group_id !== DEV && req.user.group_id !== BACKUPER) { res.sendStatus(UNAUTHORIZED).end('Access denied for workgroup: ' + isWorkGroup(req.user.group_id)); return; }
+  if (!req.query.name || !req.query.unix ) { res.sendStatus(CONFLICT).end('Bad request: required parameters missed'); return;}
+  Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user, `role: ${req.user.group_id}`, '-> BACKUP_FILE', '[', req.originalUrl, ']');
+  Backuper.getBackupFile(req.query.name, req.query.unix).then((data) => {
+    res.status(OK).send(data);
+  }).catch(err => {
+    res.status(INTERNAL_SERVER_ERROR).send({ message: err.message });
+  })
+});
+router.get('/restore-backup', corsOpt, (req: any, res: Response) => { // GET restore file
   if (!req.headers.authorization)  { res.sendStatus(UNAUTHORIZED).end('Empty authorization token'); return ; }
   if (req.user.group_id !== DEV && req.user.group_id !== BACKUPER) { res.sendStatus(UNAUTHORIZED).end('Access denied for workgroup: ' + isWorkGroup(req.user.group_id)); return; }
   if (!req.query.path || !req.query.unix) { res.sendStatus(CONFLICT).end('Bad request: required parameters missed'); return;}
