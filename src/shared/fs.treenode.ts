07cb2eabffa9bd481e710c75a3292953912034c2
neverm1ndo <it@nmnd.ru>
2022-01-17 09:14:06 +0300

    (tree-node) fix tree node builder exclusions

--- src/shared/fs.treenode.ts
+++ src/shared/fs.treenode.ts
@@ -1,4 +1,7 @@
 import { readdirSync, statSync } from 'fs';
+import { join } from 'path'
+
+const exclusions: string[] = JSON.parse(process.env.CFG_EXCLUDE!);
 
 export class TreeNode {
   public path: string;
@@ -12,14 +15,6 @@ export class TreeNode {
     this.name = nodeName;
   }
 
-  public static excludes(name: string, nodePath: string): boolean {
-    const excludes = JSON.parse(process.env.CFG_EXCLUDE!);
-    for (let e of excludes) {
-      if ((nodePath + '/' + name).includes(e)) return true;
-    };
-    return false;
-  }
-
   public static buildTree(rootPath: string, nodeName: string) {
     const root = new TreeNode(rootPath, nodeName);
 
@@ -30,19 +25,17 @@ export class TreeNode {
 
       if (currentNode) {
         const children = readdirSync(currentNode.path);
-
-        for (let child of children) {
-          if (!this.excludes(child, currentNode.path)) {
-          const childPath = `${currentNode.path}/${child}`;
+        children
+        .filter((child: string) => !(exclusions.includes(currentNode.path) || exclusions.includes(child)))
+        .forEach((child: string) => {
+          const childPath = join(currentNode.path, child);
           const childNode = new TreeNode(childPath, child);
           currentNode.items.push(childNode);
-
           if (statSync(childNode.path).isDirectory()) {
             childNode.type = 'dir';
-              stack.push(childNode);
-            }
+            stack.push(childNode);
           }
-        }
+        })
       }
     }
 
