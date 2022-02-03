0e9cacf551e3acb51240c0612c0f4e508f4c65f0
neverm1ndo <it@nmnd.ru>
2022-02-04 02:27:13 +0300

    (logs) date fixes

--- src/routes/Logs.ts
+++ src/routes/Logs.ts
@@ -24,34 +24,37 @@ router.get('/last', corsOpt, (req: any, res: any) => { // GET last lines. Defaul
   let lim = 100;
   let page = 0;
   let date = {
-    from: 'Jan 01 2000 00:00:00',
-    to: `${getTodayDate()} 23:59:59`
+    from: 'Jan 01 2000, 00:00:00',
+    to: `Dec 31 ${new Date().getFullYear()}, 23:59:59`
   };
+  console.log(getTodayDate())
   if (req.query.lim) lim = +req.query.lim;
   if (req.query.page) page = +req.query.page;
   if (req.query.filter) filter = parseSearchFilter(req.query.filter);
+  console.log(req.query)
   if (isDate(req.query.dateTo) && isDate(req.query.dateFrom)) { date = { from: req.query.dateFrom, to: req.query.dateTo }};
-  Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user, `role: ${req.user.group_id}`, '-> LINES', lim, page,' [', req.originalUrl, ']');
-  LOG_LINE.find({ date: { $gte: new Date(date.from + ' GMT+0300'), $lte: new Date(date.to + ' GMT+0300') }}, [], { sort: { unix : -1 }, limit: lim, skip: lim*page },)
+  Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user, `role: ${req.user.group_id}`, '-> LINES', lim, page,' [', req.originalUrl, ']\n└ ', JSON.stringify(req.query));
+  LOG_LINE.find({ date: { $gte: new Date(date.from + ' GMT+03:00'), $lte: new Date(date.to + ' GMT+03:00') }}, [], { sort: { unix : -1 }, limit: lim, skip: lim*page },)
   .where('process').nin(filter)
   .exec((err: any, lines: Document[]) => {
     if (err) {
       Logger.log('error', err);
-      return res.send(INTERNAL_SERVER_ERROR).end(err);
+      return res.sendStatus(INTERNAL_SERVER_ERROR).end(err);
     }
     res.send(lines);
     });
 });
 // REVIEW: same spaghetti code
 router.get('/search', corsOpt, (req: any, res: any) => { // GET Search by nickname, ip, serals
-  if (!req.headers.authorization) return res.send(UNAUTHORIZED);
+  if (!req.headers.authorization) return res.sendStatus(UNAUTHORIZED);
+  console.log(req.q)
   if (!req.query.search) {
     return res.redirect(format({ pathname: '/v2/logs/last', query: req.query }));
   }
   const query = parseSearchQuery(req.query.search);
   let date = {
-    from: 'Jan 01 2000 00:00:00',
-    to: `${getTodayDate()} 23:59:59`
+    from: 'Jan 01 2000, 00:00:00',
+    to: `Dec 31 ${new Date().getFullYear()}, 23:59:59`
   };
   let filter: string[] = [];
   let lim = 40;
@@ -73,7 +76,7 @@ router.get('/search', corsOpt, (req: any, res: any) => { // GET Search by nickna
     'geo.as': query?.as,
     'geo.ss': query?.ss,
     nickname: { $in: query?.nickname },
-    date: { $gte: new Date(date.from + ' GMT+0300'), $lte: new Date(date.to + ' GMT+0300') }
+    date: { $gte: new Date(date.from), $lte: new Date(date.to) }
   }
 
   if (!mdbq['geo.ip']?.$in) { delete mdbq['geo.ip'] };
@@ -88,7 +91,7 @@ router.get('/search', corsOpt, (req: any, res: any) => { // GET Search by nickna
   .exec((err: CallbackError, lines: Document[]) => {
     if (err) {
       Logger.log('error', err);
-      return res.send(INTERNAL_SERVER_ERROR).end(err);
+      return res.sendStatus(INTERNAL_SERVER_ERROR).end(err);
     }
     res.send(lines);
   })
