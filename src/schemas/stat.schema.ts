16148b5d0b6aa7e64ca2e848647b4fb517da71d9
neverm1ndo <it@nmnd.ru>
2022-02-07 03:05:59 +0300

    update stat schema

--- src/schemas/stat.schema.ts
+++ src/schemas/stat.schema.ts
@@ -3,7 +3,8 @@ import { model, Schema } from 'mongoose';
 const statSchema = new Schema({
   date: Date,
   label: String,
-  data: [Number]
+  data: [Number],
+  labels: [Date],
 })
 
 export const STAT = model('stat', statSchema);
