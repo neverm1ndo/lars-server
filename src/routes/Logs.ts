04ab21653e9e9b0604f7a5bf5af16182090c859a
neverm1ndo <it@nmnd.ru>
2022-02-15 17:20:56 +0300

    (logs) fix dates

--- src/routes/Logs.ts
+++ src/routes/Logs.ts
@@ -33,16 +33,16 @@ router.get('/last', corsOpt, (req: any, res: any) => { // GET last lines. Defaul
   let lim = 100;
   let page = 0;
   let date = {
-    from: +new Date('Jan 01 2000, 00:00:00'),
-    to: Date.now()
+    from: +new Date('Jan 01 2000, 00:00:00')/1000,
+    to: Date.now()*1000
   };
   if (req.query.lim) lim = +req.query.lim;
   if (req.query.page) page = +req.query.page;
   if (req.query.filter) filter = parseSearchFilter(req.query.filter);
-  if (req.query.dateFrom) date.from = +req.query.dateFrom;
-  if (req.query.dateTo) date.to = +req.query.dateTo;
+  if (req.query.dateFrom) date.from = +req.query.dateFrom/1000;
+  if (req.query.dateTo) date.to = +req.query.dateTo/1000;
   Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user, `role: ${req.user.group_id}`, '-> LINES', lim, page,' [', req.originalUrl, ']\n└ ', JSON.stringify(req.query));
-  LOG_LINE.find({ unix: { $gte: date.from/1000, $lte: date.to/1000 }}, [], { sort: { unix : -1 }, limit: lim, skip: lim*page },)
+  LOG_LINE.find({ unix: { $gte: date.from, $lte: date.to }}, [], { sort: { unix : -1 }, limit: lim, skip: lim*page },)
   .where('process').nin(filter)
   .exec((err: any, lines: Document[]) => {
     if (err) {
@@ -59,8 +59,8 @@ router.get('/search', corsOpt, (req: any, res: any) => { // GET Search by nickna
   }
   const query = parseSearchQuery(req.query.search);
   let date = {
-    from: +new Date('Jan 01 2000, 00:00:00'),
-    to: Date.now()
+    from: +new Date('Jan 01 2000, 00:00:00')/1000,
+    to: Math.round(Date.now()/1000)
   };
   let filter: string[] = [];
   let lim = 40;
@@ -68,8 +68,8 @@ router.get('/search', corsOpt, (req: any, res: any) => { // GET Search by nickna
   if (req.query.lim) lim = Number(req.query.lim);
   if (req.query.page) page = Number(req.query.page);
   if (req.query.filter) filter = parseSearchFilter(req.query.filter);
-  if (req.query.dateFrom) date.from = +req.query.dateFrom;
-  if (req.query.dateTo) date.to = +req.query.dateTo;
+  if (req.query.dateFrom) date.from = +req.query.dateFrom/1000;
+  if (req.query.dateTo) date.to = +req.query.dateTo/1000;
     Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user,`role: ${req.user.group_id}`, '-> SEARCH\n',
                '                            └ ', JSON.stringify(req.query));
   let mdbq: MDBRequest = {
@@ -79,14 +79,18 @@ router.get('/search', corsOpt, (req: any, res: any) => { // GET Search by nickna
     nickname: { $in: query?.nickname },
     process: query?.process,
     'content.message': query?.cn,
-    unix: { $gte: date.from/1000, $lte: date.to/1000 }
+    unix: { $gte: date.from, $lte: date.to }
   }
+
 
   if (!mdbq['geo.ip']?.$in) { delete mdbq['geo.ip'] };
   if (!mdbq['geo.as']) { delete mdbq['geo.as'] };
   if (!mdbq['geo.ss']) { delete mdbq['geo.ss'] };
+  if (!mdbq['process']) { delete mdbq['process'] };
   if (!mdbq['content.message']) { delete mdbq['content.message'] };
   if (!mdbq['nickname']?.$in) { delete mdbq['nickname'] };
+
+  console.log(mdbq);
 
   LOG_LINE.find(mdbq,
   [],
