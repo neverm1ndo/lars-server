65a080911fcd3d641b57e6d8299ee9a9007caaf3
neverm1ndo <it@nmnd.ru>
2022-04-25 21:31:42 +0300

    (stats) add day query param

--- src/routes/Stats.ts
+++ src/routes/Stats.ts
@@ -10,9 +10,11 @@ const router = Router();
 // const { OK, CONFLICT } = StatusCodes;
 
 router.get('/online', (req: any, res: any) => { // GET download config file
+  let date = new Date();
   // if (!req.query.path) { return res.send(CONFLICT); }
   // Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.user, `role: ${req.user.group_id}`, '-> DOWNLOAD_FILE', req.query.path, '[', req.originalUrl, ']');
-  STAT.findOne({ date: new Date().setHours(0, 0, 0, 0) }, (err: any, stat: any) => {
+  if (req.query.day) date = new Date(req.query.day);
+  STAT.findOne({ date: date.setHours(0, 0, 0, 0) }, (err: any, stat: any) => {
     if (err) return;
     res.send(stat);
   });
@@ -26,7 +28,7 @@ router.get('/chat', (req: any, res: any) => { // GET download config file
   Statsman.getChatStats(new Date(now.getFullYear(), now.getMonth()), new Date(now.getFullYear(), now.getMonth() + 1)).exec((err: any, data: any) => {
     if (err) console.log(err);
     res.send(data[0]);
-  })
+  });
 });
 
 export default router;
