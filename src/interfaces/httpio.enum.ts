5662ef87a60df6cb134dcb2248fcfe70ad75c817
neverm1ndo <it@nmnd.ru>
2022-11-06 21:03:59 +0300

    add http io enums

---
+++ src/interfaces/httpio.enum.ts
@@ -0,0 +1,18 @@
+import { IncomingMessage } from "http";
+import { Socket } from "socket.io";
+import { IDBUser } from "./user";
+
+export interface IIncomingMessage extends IncomingMessage {
+  user?: IDBUser;
+}
+
+export interface ISocket extends Socket {
+  request: IIncomingMessage;
+}
+
+export interface IHttpsOptions {
+  key: string,
+  cert: string,
+  ca?: string,
+  rejectUnauthorized: boolean
+}
