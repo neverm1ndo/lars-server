6dd62226fb8af2691eb0067a59efb2ea732724dc
neverm1ndo <it@nmnd.ru>
2022-11-06 20:58:44 +0300

    (admins) set sql queries as constants

--- src/routes/Admins.ts
+++ src/routes/Admins.ts
@@ -4,19 +4,21 @@ import { Logger } from '@shared/Logger';
 import { json } from 'express';
 import Workgroup from '@enums/workgroup.enum';
 
-import { devGuard } from '@shared/middlewares';
+import { Guards } from '@shared/guards';
 
-import { corsOpt, MSQLPool, noAvatarImageUrl } from '@shared/constants';
+import { corsOpt, MSQLPool, noAvatarImageUrl, SQLQueries } from '@shared/constants';
 
 const router = Router();
 
-const { OK, INTERNAL_SERVER_ERROR, CONFLICT, MOVED_PERMANENTLY } = StatusCodes;
+const { OK, INTERNAL_SERVER_ERROR, CONFLICT } = StatusCodes;
 const { CHALLENGER, DEV, ADMIN, CFR, MAPPER, BACKUPER } = Workgroup;
+
+const { GET_ADMIN_LIST, CHANGE_MAIN_GROUP, CHANGE_SECONDARY_GROUP } = SQLQueries;
 
 router.get('/list', (req: any, res: any) => {
   Logger.log('default', 'GET │', req.connection.remoteAddress, req.user.username,`role: ${req.user.main_group}`, '-> ADMIN_LIST [', req.originalUrl, ']');
   MSQLPool.promise()
-          .query('SELECT phpbb_users.user_id, phpbb_users.username, phpbb_users.user_avatar, phpbb_users.user_email, phpbb_users.group_id AS main_group, role.secondary_group FROM phpbb_users INNER JOIN (SELECT phpbb_user_group.user_id, MAX(phpbb_user_group.group_id) as secondary_group FROM phpbb_users, phpbb_user_group WHERE phpbb_users.group_id IN (?, ?, ?, ?, ?, ?) AND phpbb_user_group.user_id = phpbb_users.user_id GROUP BY phpbb_user_group.user_id) AS role ON role.user_id = phpbb_users.user_id', [CHALLENGER, DEV, ADMIN, MAPPER, CFR, BACKUPER])
+          .query(GET_ADMIN_LIST, [CHALLENGER, DEV, ADMIN, MAPPER, CFR, BACKUPER])
           .then(([rows]: any[]): void => {   
             for (let user of rows) {
               user.user_avatar = user.user_avatar ? `https://www.gta-liberty.ru/images/avatars/upload/${user.user_avatar}`
@@ -31,12 +33,13 @@ router.get('/list', (req: any, res: any) => {
           });
 });
 
-router.put('/change-group', devGuard, json(), corsOpt, (req: any, res: any) => {
+router.patch('/change-group', Guards.developerGuard, json(), corsOpt, (req: any, res: any) => {
   if (!req.body.id && !req.body.group) return res.sendStatus(CONFLICT);
-  Logger.log('default', 'PUT │', req.connection.remoteAddress, req.user.username,`role: ${req.user.main_group}`, '-> CHANGE_ADMIN_GROUP', `${req.body.username} : ${req.body.group}`, '[', req.originalUrl, ']');
+  
+  Logger.log('default', 'PATCH │', req.connection.remoteAddress, req.user.username,`role: ${req.user.main_group}`, '-> CHANGE_ADMIN_GROUP', `${req.body.username} : ${req.body.group}`, '[', req.originalUrl, ']');
   
   MSQLPool.promise()
-          .query('UPDATE phpbb_user_group, phpbb_users SET phpbb_user_group.group_id = ?, phpbb_users.group_id = ? WHERE phpbb_user_group.user_id = phpbb_users.user_id AND phpbb_users.user_id = ?;', [req.body.group, req.body.group, req.body.id])
+          .query(CHANGE_MAIN_GROUP, [req.body.group, req.body.group, req.body.id])
           .then((): void => {
             res.status(OK)
                .send({ status: 'OK' });
@@ -50,13 +53,13 @@ router.put('/change-group', devGuard, json(), corsOpt, (req: any, res: any) => {
           });
 });
 
-router.put('/change-secondary-group', devGuard, json(), (req: any, res: any) => {
+router.patch('/change-secondary-group', Guards.developerGuard, json(), (req: any, res: any) => {
   if (!req.body.id && !req.body.group) return res.sendStatus(CONFLICT);
   
-  Logger.log('default', 'PUT │', req.connection.remoteAddress, req.user.username,`role: ${req.user.main_group}`, '-> CHANGE_SECONDARY_ADMIN_GROUP', `${req.body.username} : ${req.body.group}`, '[', req.originalUrl, ']');
+  Logger.log('default', 'PATCH │', req.connection.remoteAddress, req.user.username,`role: ${req.user.main_group}`, '-> CHANGE_SECONDARY_ADMIN_GROUP', `${req.body.username} : ${req.body.group}`, '[', req.originalUrl, ']');
   
   MSQLPool.promise()
-          .query('UPDATE phpbb_user_group INNER JOIN (SELECT MAX(group_id) as group_id FROM phpbb_user_group WHERE group_id BETWEEN 9 AND 14 AND user_id = ? GROUP BY user_id) AS secondary_group SET phpbb_user_group.group_id = ? WHERE phpbb_user_group.group_id = secondary_group.group_id AND user_id = ?', [req.body.id, req.body.group, req.body.id])
+          .query(CHANGE_SECONDARY_GROUP, [req.body.id, req.body.group, req.body.id])
           .then((): void => {
             res.status(OK)
                .send({ status: 'OK' });
