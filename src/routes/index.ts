4f51a443ea8c8e6164265ec835b3f208e37511af
neverm1ndo <it@nmnd.ru>
2022-11-06 21:00:58 +0300

    (routes) routes rebuild

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,22 +8,25 @@ import UtilsRouter from './Utils';
 import AdminsRouter from './Admins';
 import BackupsRouter from './Backups';
 import StatsRouter from './Stats';
-import { rejectUnauthorized, backuperGuard, devGuard, cfrGuard, mapGuard } from '@shared/middlewares';
-import { corsOpt } from '@shared/constants';
+import { Guards } from '@shared/guards';
+import { corsOpt as cors } from '@shared/constants';
 
 // Init router and path
 const router = Router();
+      router.use('/auth', cors, LoginRouter);
 
-// Add sub-routes
-router.use('/login', LoginRouter);
-router.use('/bans', BansRouter, corsOpt, rejectUnauthorized);
-router.use('/logs', corsOpt, rejectUnauthorized, LogsRouter);
-router.use('/maps', corsOpt, rejectUnauthorized, mapGuard, MapsRouter);
-router.use('/configs', corsOpt, rejectUnauthorized, cfrGuard, ConfigsRouter);
-router.use('/admins', corsOpt, rejectUnauthorized, AdminsRouter);
-router.use('/backups', corsOpt, rejectUnauthorized, backuperGuard, BackupsRouter);
-router.use('/stats', corsOpt, rejectUnauthorized, StatsRouter);
-router.use('/utils', corsOpt, rejectUnauthorized, devGuard, UtilsRouter);
+// LARS API router 
+const larsRouter = Router();
+      larsRouter.use('/bans', BansRouter);
+      larsRouter.use('/logs', LogsRouter);
+      larsRouter.use('/maps', Guards.mapperGuard, MapsRouter);
+      larsRouter.use('/configs', Guards.configuratorGuard, ConfigsRouter);
+      larsRouter.use('/admins', AdminsRouter);
+      larsRouter.use('/backups', Guards.backuperGuard, BackupsRouter);
+      larsRouter.use('/stats', StatsRouter);
+      larsRouter.use('/utils', Guards.developerGuard, UtilsRouter);
+
+router.use('/lars', cors, Guards.rejectUnauthorized, larsRouter);
 
 // Export the base-router
 export default router;
