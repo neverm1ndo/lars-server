c58978e1f74860969785f9c2fa273536caa98b4b
neverm1ndo <it@nmnd.ru>
2022-11-17 18:36:22 +0300

    (treenode) remove blockings

--- src/shared/fs.treenode.ts
+++ src/shared/fs.treenode.ts
@@ -1,48 +1,58 @@
-import { readdirSync, statSync } from 'fs';
-import { join, normalize } from 'path'
+import { readdir, stat } from 'fs/promises';
+import { Stats } from 'fs';
+import { join, normalize } from 'path';
 
 const exclusions: string[] = JSON.parse(process.env.CFG_EXCLUDE!);
 
 export class TreeNode {
+  public hasChildren?: boolean;
   public path: string;
-  public items: Array<TreeNode>;
+  public items: Array<TreeNode> = [];
   public type: string = 'file';
   public name: string = 'configs';
+  public expanded?: boolean = false;
 
   constructor(path: string, nodeName: string) {
     this.path = path;
-    this.items = [];
     this.name = nodeName;
   }
 
-  static isExcludedDirPath(path: string): boolean {
+  private static _isExcludedDirPath(path: string): boolean {
     for (let i = 0; i < exclusions.length; i++) {
       if (path === (normalize(exclusions[i]))) return true;
     }
     return false;
   }
 
-  public static buildTree(rootPath: string, nodeName: string) {
+  public static async buildTree(rootPath: string, nodeName: string): Promise<TreeNode> {
     const root = new TreeNode(rootPath, nodeName);
+          root.expanded = true;
 
-    const stack = [root];
+    const stack: TreeNode[] = [root];
 
     while (stack.length) {
       const currentNode = stack.pop();
-      if (currentNode) {
-        const children = readdirSync(currentNode.path);
-        children.filter((child: string) => !this.isExcludedDirPath(currentNode.path) && !exclusions.includes(child))
-                .forEach((child: string) => {
-                  const childPath = join(currentNode.path, child);
-                  const childNode = new TreeNode(childPath, child);
-                  if (!this.isExcludedDirPath(childNode.path)) {
-                    currentNode.items.push(childNode);
-                    if (statSync(childNode.path).isDirectory()) {
-                      childNode.type = 'dir';
-                      stack.push(childNode);
-                    }
-                  }
-                });
+      
+      if (currentNode) {    
+        let children: string[] = await readdir(currentNode.path);
+
+        children = children.filter((child: string) => !this._isExcludedDirPath(currentNode.path) && !exclusions.includes(child));
+        
+        for (let child of children) {
+
+          const childPath: string = join(currentNode.path, child);
+          const childNode: TreeNode = new TreeNode(childPath, child);
+
+          currentNode.items.push(childNode);
+
+          const nodeStats: Stats = await stat(childNode.path);
+
+          if (nodeStats.isDirectory()) {
+            childNode.type = 'dir';
+            stack.push(childNode);
+          }
+        }
+
         currentNode.items = currentNode.items.sort((a, b) => {
           if (a.type == 'dir' && b.type == 'file') return -1;
           if (a.type == 'file' && b.type == 'dir') return 1;
@@ -50,6 +60,7 @@ export class TreeNode {
         });
       }
     }
+
     return root;
   }
 }
