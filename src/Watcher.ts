e21a4d28a8b6998e28efaf05a88b093c3a8886ef
neverm1ndo <it@nmnd.ru>
2022-11-17 18:35:12 +0300

    (watcher) watcher rework

--- src/Watcher.ts
+++ src/Watcher.ts
@@ -1,82 +1,81 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import { createReadStream, ReadStream, Stats } from 'fs';
+import { stat, readdir } from 'fs/promises';
+import { Stream } from 'stream';
 import chokidar from 'chokidar';
-import dotenv from 'dotenv';
+import * as path from 'path';
 import bufferSplit from 'buffer-split';
-import { Logger } from '@shared/Logger';
-import _ from 'lodash';
-
-import { Observable } from 'rxjs';
-
-dotenv.config({ path:path.resolve(process.cwd(), 'server/.env') });
 
 export class Watcher {
-  private lines: number = 0;
-  private _current: string = '';
-  public lastFile$: Observable<string> = new Observable((subscriber) => {
-              fs.readdir(path.join(__dirname, `./log/${new Date().getFullYear()}/09`), (err: any, files: string[]) => {
-              if (err) {
-                subscriber.error('Cant scan directory');
-                Logger.log('error', 'Cant scan directory', err);
-              } else {
-                subscriber.next(files[files.length]);
-              };
-            });
-        });
-  public result$: Observable<Buffer>;
-  public watcher: any = chokidar.watch(process.env.LOGS_PATH!, {
+  
+  private _fsWatcher: chokidar.FSWatcher = chokidar.watch(process.env.LOGS_PATH!, {
     ignored: /(^|[\/\\])\../,
-    persistent: true
+    persistent: true,
   });
 
-  private convert(str: number): string {
-    const pad = '00';
-    return pad.substring(0, pad.length - str.toString().length) + str.toString();
-  }
-
-  getLogFIle() {
-    const date = new Date();
-    let logpath = path.join(process.env.LOGS_PATH!, this.convert(date.getMonth() + 1), `${date.getFullYear()}${this.convert(date.getMonth()+1)}${this.convert(date.getDate())}.log`);
-    
-    if (process.env.NODE_ENV === 'development') logpath = path.join(process.env.LOGS_PATH!, '20200931.log')
-    
-    fs.readFile(logpath, (err: NodeJS.ErrnoException | null, buffer: Buffer) => {
-      if (err) { Logger.log('error', err) }
-      const delim = Buffer.from('\n');
-      const splited = bufferSplit(buffer, delim);
-      this.lines = splited.length;
-      this._current = logpath;
-    });
-  }
-
-  constructor() {
-    this.getLogFIle();
-    this.result$ = new Observable<Buffer>((subscriber) => {
-      if (!this.watcher) return;
-      this.watcher.on('change', ( filepath: string ) => {
-        if (this._current !== filepath) {
-          this.lines = 0;
-          this._current = filepath;
-        }
-        fs.readFile(path.resolve(process.cwd(), filepath), (err, buffer: Buffer) => {
-          if (err) {
-            Logger.log('error', err);
-            return;
-          } else {
-            const delim = Buffer.from('\n');
-            const splited = bufferSplit(buffer, delim);
-            const newLines = splited.length - 1 - this.lines;
-            if (splited.length > 2) {
-              _.takeRight(splited, newLines + 2).forEach((line: Buffer) => {
-                subscriber.next(line);
-              })
-            } else {
-              subscriber.next(splited[0]);
-            }
-            this.lines = splited.length;
-          }
+  private _bytes: number = 0;
+  private _stream: Stream = new Stream();
+  
+  public overwatch(): Stream {
+    this._getLastLogFileStat()
+        .then((stats: Stats) => {
+          this._bytes = stats.size + 2;
         });
-      });
-    });
+    this._fsWatcher.on('change', (path, stats) => this._fsWatcherHandler(path, stats));
+    this._fsWatcher.on('error', console.error);
+    return this._stream;
   }
+
+  private async _getLastLogFileStat(): Promise<Stats> {
+    
+    const now: Date = new Date();
+    
+    const [date, month, year] = [
+      now.getDate().toString(), 
+     (now.getMonth() + 1).toString(), 
+      now.getFullYear().toString(),
+    ].map((val: string) => '00'.substring(0, 2 - val.length) + val);
+   
+    let filepath = path.join(process.env.LOGS_PATH!, year, month,`${year}${month}${date}.log`);
+
+    try {
+      return await stat(filepath);
+    } catch(error) {
+      const dirpath: string = path.dirname(filepath);
+      const dir: string[] = await readdir(dirpath);
+      
+      filepath = path.join(dirpath, dir[dir.length - 1]);
+      
+      return await stat(filepath);
+    }
+  }
+
+  private _fsWatcherHandler(path: string, stats?: Stats | undefined): void {
+    
+    if (!stats) return;
+    
+    if (stats.size <= this._bytes) {
+      this._bytes = 0;
+    }
+
+    const stream: ReadStream = createReadStream(path, {
+      start: this._bytes,
+      end: stats.size - 1,
+    });
+    
+    stream.on('readable', () => {
+      const data: Buffer | null = stream.read();
+      
+      if (!data) return stream.destroy();
+      
+      const newLines: Buffer[] = bufferSplit(data, Buffer.from('\n'));
+
+      for (let line of newLines) {
+        this._stream.emit('data', line);
+      }
+    
+    });
+    
+    this._bytes = stats.size + 2;  
+  }
+
 }
