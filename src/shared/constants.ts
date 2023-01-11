7e34f2530fd1c06959278cd1205163af9a2cded9
neverm1ndo <it@nmnd.ru>
2023-01-11 21:46:51 +0300

    add allowed headers to cors options

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -82,6 +82,14 @@ export const MSQLPool = createPool({
 const whitelist = ['http://localhost'];
 
 export const CORSoptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+    'Authorization'
+  ],
     credentials: true,
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
     origin: '*',
