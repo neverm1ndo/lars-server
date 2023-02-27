f854b282b17628d6ff60a7cd7c57778a39fba371
neverm1ndo <it@nmnd.ru>
2023-02-27 11:05:53 +0300

    fix (parser2) fix cli lex rule regexp

--- src/Parser2.ts
+++ src/Parser2.ts
@@ -27,7 +27,7 @@ export class Parser2 {
                 ["\\>", "return '>';"],
                 ["\\(", "return '(';"],
                 ["\\)", "return ')';"],
-                ["[0-9]\\.[0-9](\\.|[a-zA-Z]+)\\.?[0-9]?\\.?([A-Z]+)?(\\-[a-zA-Z0-9]+)?", "return 'CLI';"],
+                ["[0-9]\\.[0-9](\\.|[a-zA-Z]+)\\.?[0-9]?([a-zA-Z]+)?\\.?([A-Z]+)?(\\-[a-zA-Z0-9]+)?", "return 'CLI';"],
                 ["{int}{frac}?\\b", "return 'NUMBER';"],
                 ["\'(?:\\\\[\"bfnrt/\\\\]|\\\\u[a-fA-F0-9]{4}|[^\"\\\\])*\'", "yytext = yytext.substr(1,yyleng-2); return 'MESSAGE';"],
                 ["(?=.*[a-zA-Zа-яА-Я])(?=.*[0-9])[a-zA-Zа-яА-Я0-9\\_\\!\\?\\.\\-\\s\\[\\]\\|]+|[a-zA-Zа-яА-Я_\\.\\-]+", "return 'STRING';"],
