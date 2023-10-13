cf5579e4a56c80d1f9741a1122cdab0b3f571b0e
neverm1ndo <it@nmnd.ru>
2023-10-14 00:30:35 +0300

    update build script

--- build.ts
+++ build.ts
@@ -1,60 +1,109 @@
 /**
  * Remove old files, copy front-end ones.
  */
-
-import * as fs from 'fs-extra';
+import path from 'path';
+import fs from 'fs/promises';
 import Logger from 'jet-logger';
-import * as childProcess from 'child_process';
+import { ExecException, exec } from 'child_process';
 
 // Setup logger
-const logger = new Logger();
-logger.timestamp = false;
 
+const config = {
+    env: 'pre-start/env/',
+    dist: {
+        path: './dist/',
+    },
+    src: {
+        path: './src/',
+    },
+    build: {
+        cmd: 'tsc --build tsconfig.prod.json',
+        cwd: './'
+    },
+};
 
+process.stdout.write(`\x1b[36m
+     ██       █████  ██████  ███████
+     ██      ██   ██ ██   ██ ██
+     ██      ███████ ██████  ███████
+     ██      ██   ██ ██   ██      ██
+     ███████ ██   ██ ██   ██ ███████
+     Server build script
+\x1b[0m\n`);
 
-
-(async () => {
+(async function build(config) {
     try {
+        const start = Date.now();
+
         // Remove current build
-        await remove('./dist/');
+        console.info(' --- Removing current build...');
+        await fs.rm(config.dist.path, { recursive: true, force: true });
+
+        console.info(' --- removed!\n');
+
+        // Restore dir structure
+        const envDir = path.join(config.dist.path, config.env);
+        await fs.mkdir(envDir, { recursive:  true });
+        
         // Copy production env file
-        await copy('./src/pre-start/env/production.env', './dist/pre-start/env/production.env');
+        const prodEnvFile: string = path.join(config.env, 'production.env');
+
+        const [envSrc, envDest] = [
+            config.src.path,
+            config.dist.path
+        ].map((libpath) => path.join(libpath, prodEnvFile));
+
+        await fs.copyFile(envSrc, envDest);
+        
         // Copy back-end files
-        await exec('tsc --build tsconfig.prod.json', './')
-    } catch (err) {
-        logger.err(err);
+        console.info(' --- Transpiling *.ts source files...');
+        await new Promise<void>((resolve, reject) => {
+            return exec(config.build.cmd, { cwd: config.build.cwd }, (err: ExecException | null, stdout: string, stderr: string) => {
+                if (!!stdout) {
+                    console.info(stdout);
+                }
+                if (!!stderr) {
+                    console.warn(stderr);
+                }
+                return void(!!err ? reject(err) : resolve());
+            });
+        });
+
+        const fin = Date.now();
+
+        console.info(' --- \x1b[32mOK!\n\t\x1b[36mAssembled in \x1b[33m' + duration(fin - start) + '\x1b[0m');
+
+   } catch(err) {
+        console.error(err);
+   }
+})(config);
+
+function duration(d: number): string {
+    const format = {
+        _ms: d,
+        get ms() {
+            return d % 1000;
+        },
+        get s() {
+            return (d - this.ms) / 1000 
+        },
+        get m() { 
+            return this.s / 60;
+        },
+    };
+
+    const duration: Record<string, number> = {
+        m: Math.floor(format.m),
+        s: 0,
+        ms: format.ms,
+    };
+    duration.s = format.s - duration.m*60;
+
+    let str: string[] = [];
+
+    for (let [scalar, val] of Object.entries(duration)) {
+        if (val > 0) str.push(val + scalar);
     }
-})();
 
-
-function remove(loc: string): Promise<void> {
-    return new Promise((res, rej) => {
-        return fs.remove(loc, (err) => {
-            return (!!err ? rej(err) : res());
-        });
-    });
-}
-
-
-function copy(src: string, dest: string): Promise<void> {
-    return new Promise((res, rej) => {
-        return fs.copy(src, dest, (err) => {
-            return (!!err ? rej(err) : res());
-        });
-    });
-}
-
-
-function exec(cmd: string, loc: string): Promise<void> {
-    return new Promise((res, rej) => {
-        return childProcess.exec(cmd, {cwd: loc}, (err, stdout, stderr) => {
-            if (!!stdout) {
-                logger.info(stdout);
-            }
-            if (!!stderr) {
-                logger.warn(stderr);
-            }
-            return (!!err ? rej(err) : res());
-        });
-    });
+    return str.join(' ');
 }
