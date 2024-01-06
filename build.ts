b2ed46c2f7222b74b2737bfd04304d90582bd309
neverm1ndo <it@nmnd.ru>
2024-01-06 16:01:15 +0300

    chore update builder

--- build.ts
+++ build.ts
@@ -3,7 +3,8 @@
  */
 import path from 'path';
 import fs from 'fs/promises';
-import { ExecException, exec } from 'child_process';
+import { ExecException, ExecOptions, exec } from 'child_process';
+import { get, isFunction } from 'lodash';
 
 const config = {
     env: 'pre-start/env/',
@@ -17,7 +18,44 @@ const config = {
         cmd: 'tsc --build tsconfig.prod.json',
         cwd: './'
     },
+    postbuild: {
+        peg: (name: string) => `pegjs -o ${path.join('./dist/parser/', name, name + '.parser.js')} --cache --trace ./src/parser/grammar/${name}.grammar.pegjs`,
+    },
 };
+
+const CONSOLE_MESSAGE = {
+    RM: {
+        CURRENT: 'Removing current build',
+        SUCCESS: 'Removed',
+    },
+    BUILD: {
+        PROCESSING: 'Transpiling *.ts source files',
+        SUCCESS: (fin: number, start: number) => `${color.green('OK!')}\n\t${color.blue('Assembled in')} ${color.yellow(duration(fin - start))}`
+    },
+    POST_BUILD: {
+        PEG_GEN: (curr?: number, all?: number) => `Generating parsers ${curr ?? 0}/${all ?? 0}`
+    },
+    get: function(field: string, ...args: unknown[]) {
+        const message = get(this, field);
+        return `${field.split('.').map((e) =>`[${e}]`).join('')} ` + (isFunction(message) ? message.call(null, ...args) : message);
+    }
+};
+
+const color = {
+    get _codes() {
+        return {
+            green: 32,
+            yellow: 33,
+            blue: 36,
+            clear: 0
+        };
+    },
+    _colorize: function(code: number, message: string) { return this._compileCode(code) + message + this._compileCode(0)},
+    _compileCode: function(code: number) { return `\x1b[${code}m`; },
+    blue: function(str: string) { return this._colorize(this._codes.blue, str); },
+    green: function(str: string) { return this._colorize(this._codes.green, str); },
+    yellow: function(str: string) { return this._colorize(this._codes.yellow, str); },
+}
 
 process.stdout.write(`\x1b[36m
      ██       █████  ██████  ███████
@@ -33,10 +71,10 @@ process.stdout.write(`\x1b[36m
         const start = Date.now();
 
         // Remove current build
-        console.info(' --- Removing current build...');
+        console.info(CONSOLE_MESSAGE.get('RM.CURRENT'));
         await fs.rm(config.dist.path, { recursive: true, force: true });
 
-        console.info(' --- removed!\n');
+        console.info(CONSOLE_MESSAGE.get('RM.SUCCESS'));
 
         // Restore dir structure
         const envDir = path.join(config.dist.path, config.env);
@@ -53,22 +91,23 @@ process.stdout.write(`\x1b[36m
         await fs.copyFile(envSrc, envDest);
         
         // Copy back-end files
-        console.info(' --- Transpiling *.ts source files...');
-        await new Promise<void>((resolve, reject) => {
-            return exec(config.build.cmd, { cwd: config.build.cwd }, (err: ExecException | null, stdout: string, stderr: string) => {
-                if (!!stdout) {
-                    console.info(stdout);
-                }
-                if (!!stderr) {
-                    console.warn(stderr);
-                }
-                return void(!!err ? reject(err) : resolve());
-            });
-        });
+        console.info(CONSOLE_MESSAGE.get('BUILD.PROCESSING'));
+
+        await execCmd(config.build.cmd, { cwd: config.build.cwd });
 
         const fin = Date.now();
 
-        console.info(' --- \x1b[32mOK!\n\t\x1b[36mAssembled in \x1b[33m' + duration(fin - start) + '\x1b[0m');
+        console.info(CONSOLE_MESSAGE.get('BUILD.SUCCESS', fin, start));
+
+        // Postbuild gen parsers
+        let current = 0;
+
+        const grammars = ['query'];
+        
+        for (let grammar of grammars) {
+            console.info(CONSOLE_MESSAGE.get('POST_BUILD.PEG_GEN', current++, grammars.length));
+            await execCmd(config.postbuild.peg(grammar), { cwd: config.build.cwd });
+        }
 
    } catch(err) {
         console.error(err);
@@ -103,4 +142,18 @@ function duration(d: number): string {
     }
 
     return str.join(' ');
+}
+
+function execCmd(cmd: string, options: ExecOptions) {
+    return new Promise<void>((resolve, reject) => {
+        return exec(cmd, options, (err: ExecException | null, stdout: string, stderr: string) => {
+            if (!!stdout) {
+                console.info(stdout);
+            }
+            if (!!stderr) {
+                console.warn(stderr);
+            }
+            return void(!!err ? reject(err) : resolve());
+        });
+    });
 }
