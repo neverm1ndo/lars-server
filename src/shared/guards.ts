1bdcddfa9d3aa7c5af99c809fdd97641a9ccf2d9
neverm1ndo <it@nmnd.ru>
2024-02-11 00:04:48 +0300

    hotfix(shared/guards) fixed guards for new permissions format

--- src/shared/guards.ts
+++ src/shared/guards.ts
@@ -2,14 +2,21 @@ import { Request, Response, NextFunction } from 'express';
 import { isWorkGroup } from '@shared/functions';
 import StatusCodes from 'http-status-codes';
 import Workgroup from '@enums/workgroup.enum';
+import { intersection } from 'lodash';
 
 interface IUser extends Express.User {
   main_group: Workgroup;
-  secondary_group: Workgroup;
+  permissions: Workgroup[];
 }
 
 const { DEV, MAPPER, CFR, BACKUPER } = Workgroup;
 const { UNAUTHORIZED } = StatusCodes;
+
+const ALLOWED_GROUPS = {
+  BACKUPS: [DEV, BACKUPER],
+  MAPS: [DEV, MAPPER],
+  CONFIGURATIONS: [DEV, CFR]
+}
 
 export namespace Guards {
   export const rejectUnauthorized = function (req: Request, res: Response, next: NextFunction) {
@@ -17,30 +24,30 @@ export namespace Guards {
       next();
   };
   
-  export const backuperGuard = function(req: any, res: Response, next: NextFunction) {
-    if (!req.user) return res.sendStatus(UNAUTHORIZED);
-    if (req.user.main_group !== DEV && req.user.secondary_group !== BACKUPER) { res.status(UNAUTHORIZED).send('Access denied for your workgroup: ' + isWorkGroup(req.user.main_group)); return; }
+  export const backuperGuard = function({ user }: any, res: Response, next: NextFunction) {
+    if (!user) return res.sendStatus(UNAUTHORIZED);
+    if (!intersection(user.permissions, ALLOWED_GROUPS.BACKUPS).length) { res.status(UNAUTHORIZED).send('Access denied for your workgroup: ' + isWorkGroup(user.main_group)); return; }
     next();
   };
   
-  export const developerGuard = function(req: any, res: Response, next: NextFunction) {
-    if (!req.user) return res.sendStatus(UNAUTHORIZED);
-    if (req.user.main_group !== DEV) { res.status(UNAUTHORIZED).send('Access denied for your workgroup: ' + isWorkGroup(req.user.main_group)); return; }
+  export const developerGuard = function({ user }: any, res: Response, next: NextFunction) {
+    if (!user) return res.sendStatus(UNAUTHORIZED);
+    if (user.main_group !== DEV) { res.status(UNAUTHORIZED).send('Access denied for your workgroup: ' + isWorkGroup(user.main_group)); return; }
     next();
   };
   
-  export const mapperGuard = function(req: any, res: Response, next: NextFunction) {
-    if (!req.user) return res.sendStatus(UNAUTHORIZED);
-    if (req.user.main_group !== DEV && req.user.secondary_group !== MAPPER && req.user.secondary_group !== BACKUPER) { 
-      res.status(UNAUTHORIZED).send('Access denied for your workgroup: ' + isWorkGroup(req.user.main_group)); 
+  export const mapperGuard = function({ user }: any, res: Response, next: NextFunction) {
+    if (!user) return res.sendStatus(UNAUTHORIZED);
+    if (!intersection(user.permissions, ALLOWED_GROUPS.MAPS).length) { 
+      res.status(UNAUTHORIZED).send('Access denied for your workgroup: ' + isWorkGroup(user.main_group)); 
       return; 
     }
     next();
   };
   
-  export const configuratorGuard = function(req: any, res: Response, next: NextFunction) {
-    if (!req.user) return res.sendStatus(UNAUTHORIZED);
-    if (req.user.main_group !== DEV && req.user.secondary_group !== CFR && req.user.secondary_group !== BACKUPER) { res.status(UNAUTHORIZED).send('Access denied for your workgroup: ' + isWorkGroup(req.user.main_group)); return; }
+  export const configuratorGuard = function({ user }: any, res: Response, next: NextFunction) {
+    if (!user) return res.sendStatus(UNAUTHORIZED);
+    if (!intersection(user.permissions, ALLOWED_GROUPS.CONFIGURATIONS).length) { res.status(UNAUTHORIZED).send('Access denied for your workgroup: ' + isWorkGroup(user.main_group)); return; }
     next();
   };
 };
