0ac2c6b644a65a3c9fc352b0c2050d5387b48334
neverm1ndo <it@nmnd.ru>
2025-05-02 20:00:45 +0300

    Initial commit

---
+++ apps/lars-server-e2e/jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  displayName: 'lars-server-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/lars-server-e2e',
+};
